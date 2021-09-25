2c08d22a227f6f353e175237fc2a4698581faed8
volodink <volodin.konstantin@gmail.com>
2021-09-25 13:02:56 +0300

    Результат работы на Практическом занятии №2.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
 console.log("Hello, world!");
 
-_root_ = document.getElementById("application")
-console.log(_root_);
+B = document.getElementById("application")
 
-for (let i = 0; i < 10; i++) {
-    let doge_img = document.createElement("img")
-    doge_img.setAttribute("src", "img/doge.jpg")
-    console.log(doge_img);
+let input_a = document.createElement("input");
+input_a.setAttribute("id", "input_a");
+B.append(input_a);
 
-    _root_.append(doge_img)
+let input_b = document.createElement("input");
+input_b.setAttribute("id", "input_b");
+B.append(input_b);
+
+let result_button = document.createElement("button");
+result_button.innerText = "Get result";
+result_button.onclick = () => {
+    // alert("Click!!!");
+    let in_a = document.getElementById("input_a").value;
+    let in_b = document.getElementById("input_b").value;
+    console.log(parseFloat(in_a) + parseFloat(in_b));
+    let res = document.getElementById("result")
+    res.innerText = parseFloat(in_a) + parseFloat(in_b);
 }
-// console.log(i);
+B.append(result_button);
 
+let result_ti = document.createElement("h3");
+result_ti.setAttribute("id", "result")
+result_ti.innerText = "Нет значения";
+B.append(result_ti);
